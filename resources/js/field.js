eca9c9b56bfc8efdcf0444ca1e3bc38c23e8f2c1
Jason Wright <jwright@speareducation.com>
2025-05-15 11:22:26 -0700

    updated field.js nova.booting signature

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,3 @@
-Nova.booting((Vue, router) => {
-    Vue.component('index-order-field', require('./components/IndexField'));
+Nova.booting((app, router) => {
+    app.component('index-order-field', require('./components/IndexField'));
 })
